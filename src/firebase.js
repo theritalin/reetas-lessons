a047c5daa901bedeef4a2e92ddd8513db43df930
thertln <fatihbasacek@gmail.com>
2024-10-15 23:48:21 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9l6Xff33j5plpMTCSpUSqEj2YgqRBA3Y",
-  authDomain: "reetas-lesson.firebaseapp.com",
-  projectId: "reetas-lesson",
-  storageBucket: "reetas-lesson.appspot.com",
-  messagingSenderId: "1038938267062",
-  appId: "1:1038938267062:web:9df816e73ce56eceed7517",
-  measurementId: "G-K2J3EH0QHW",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
